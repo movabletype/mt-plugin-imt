128c44cc2d57a67eac034c6e4801a8d37e713f06
Finn Smith <finn@timeghost.net>
2010-10-12 17:29:35 -0400

    Fix license headers.

--- mt-static/plugins/iMT/js/tc.js
+++ mt-static/plugins/iMT/js/tc.js
@@ -1,10 +1,14 @@
-/*
-Copyright 2003 Six Apart. This code cannot be redistributed without
-permission from www.sixapart.com.
-
-$Id$
-*/
-
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2010 Six Apart Ltd.
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of version 2 of the GNU General Public License as published
+// by the Free Software Foundation, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// version 2 for more details.  You should have received a copy of the GNU
+// General Public License version 2 along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
 
 /*
 --------------------------------------------------------------------------------
